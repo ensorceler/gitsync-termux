143b954ed8e96160a1817b5b54996846b21b2f4b
ensorceler <ensorceler27@gmail.com>
2022-10-09 21:19:07 +0530

    updated script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-
-user=$1
-repo=$2
-
-
-
 path="/data/data/com.termux/files/home/storage/shared/Documents/my_github_repos"
 
-if [[ ! -d $path]]
+echo -e "\033[0;31m Github repo: \e[0m "
+echo -e -n "\033[0;32m Enter your github username: \e[0m" 
+read user_name
+echo -e -n "\033[0;32m Enter your github reponame: \e[0m"
+read repo_name 
+
+if [ -z "$user_name" ] || [ -z "$repo_name" ]
 then
-echo "path doesn't exist, creating directory"
-mkdir -p "$path"
+    echo "empty github username or repository name, Please provider proper values"
+    exit 1
+fi
+
+if [[ ! -d $path ]]
+then
+    echo "path does not exist , creating directory"
+    mkdir -p "$path"
 fi
 
 cd $path
 
 git clone "git@github.com:$user/$repo 
 echo "repo cloned successfully"
-
-
