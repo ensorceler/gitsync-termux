bff366bd3a74df596b32d1ceacd065e41be33b43
ensorceler <ensorceler27@gmail.com>
2022-10-09 17:59:16 +0530

    initial commit

---
+++ install_packages.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+echo "Checking everything works or not in termux"
---
+++ setup_ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "$1"
+
+
+#ssh-keygen -t ed25519 -C "sussyAmogus@goomail.com" -N "" -f ~/.ssh/github_keys 
+
+
+
+
