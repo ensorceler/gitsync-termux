243c4ef2ffd89d667c1351b46cf78af1633c7074
ensorceler <ensorceler27@gmail.com>
2022-10-09 20:07:11 +0530

    added script.sh

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+user=$1
+repo=$2
+
+
+PATH="/data/data/com.termux/files/home/storage/shared/Documents/my_github_repos"
+
+if [[ ! -d $PATH ]]
+then
+    echo "path doesn't exist, creating directory"
+    mkdir -p $PATH
+fi
+
+cd $PATH
+
+git clone "git@github.com:$user/$repo 
+echo "repo cloned successfully"
+
+
--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-echo "$1"
+user_email=$1
+if [[ -z "$1" ]]
+then 
+    echo "Please pass the required argument i.g ./setup_ssh <your-email>"
+    exit 1
+fi
 
+: '
 
-#ssh-keygen -t ed25519 -C "sussyAmogus@goomail.com" -N "" -f ~/.ssh/github_keys 
+ssh-keygen: The command-line tool used for creating a new pair of SSH keys. You can see its flags with ssh-keygen help
+-t ed25519: The -t flag is used to indicate the algorithm used to create the digital signature of the key pair. If your system supports it, ed25519 is the best algorithm you can use to create SSH key pairs.
+-C “email”: The -c flag is used to provide a custom comment at the end of the public key, which usually is the email or identification of the creator of the key pair.
+- N "": the -N flag is used to provide a passphrase. We are providing an empty passphrase. You can configure that to your liking.
+-f : the -f flag is used to provide a file path where the generated keys will be stored. 
+'
+
+: '
+ssh-keygen -t ed25519 -C "$1" -N "" -f ~/.ssh/github_keys 
+
+eval `ssh-agent`
+
+ssh-add ~/.ssh/github_keys
+
+'
+
+echo -e "\e[31m Copy this to part to the clipboard and paste into New SSH Key\e[0m\n"
+
+cat ~/.ssh/github_keys.pub
 
 
 
