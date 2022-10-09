23b9a1464d405061e0d9bb6e1f42b54f63d8d3ee
ensorceler <ensorceler27@gmail.com>
2022-10-10 00:41:29 +0530

    ssh-keygen file path default

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -22,13 +22,10 @@ ssh-keygen: The command-line tool used for creating a new pair of SSH keys. You 
 -t ed25519: The -t flag is used to indicate the algorithm used to create the digital signature of the key pair. If your system supports it, ed25519 is the best algorithm you can use to create SSH key pairs.
 -C “email”: The -c flag is used to provide a custom comment at the end of the public key, which usually is the email or identification of the creator of the key pair.
 - N "": the -N flag is used to provide a passphrase. We are providing an empty passphrase. You can configure that to your liking.
--f : the -f flag is used to provide a file path where the generated keys will be stored. 
 '
 
-ssh-keygen -t ed25519 -C "$email" -N "" -f ~/.ssh/github_keys 
+ssh-keygen -t ed25519 -C "$email" -N "" -f ~/.ssh/id_25519
 
-eval `ssh-agent -s`
-ssh-add ~/.ssh/github_keys
 
 if [ $? -eq 0 ]
 then 
@@ -36,7 +33,7 @@ then
 fi
 echo -e "\e[31m Copy this to part to the clipboard and paste into New SSH Key\e[0m\n"
 
-cat ~/.ssh/github_keys.pub
+cat ~/.ssh/id_25519.pub
 
 
 
